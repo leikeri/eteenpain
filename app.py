0d5c267aa7523231f6d6a25b68b2bcdb6cfdb992
leikeri <aki.jarvenpaa@gmail.com>
2022-11-25 15:04:50 +0100

    admin_html formats updated

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ BUCKET_NAME = 'galleryeteenpain'
 
 app = Flask(__name__)
 
-images = ["/static/viiri.jpg"]
+images = ["/static/viiri.jpg", "/static/malaga_ryhma.jpg"]
 
 
 @app.route('/')
@@ -83,9 +83,9 @@ def tapahtumat_home():
     folders = dict()
     # 1. authenticate  2. Get temp access token...
     # testing cognito funs
-    user_pool_id = "us-east-1:0719f112-2e98-47b2-b3e0-a7d285b26f2f"
-    client_id = "42op390n44fc9nsjbru7430t58"  # tama on eteenpain appin id numero
-    auth_tool = aws2.CognitoIdentityProviderWrapper(boto3.client('cognito-idp', 'us-east-1'), user_pool_id=user_pool_id, client_id=client_id)
+    # user_pool_id = "us-east-1:0719f112-2e98-47b2-b3e0-a7d285b26f2f"
+    # client_id = "42op390n44fc9nsjbru7430t58"  # tama on eteenpain appin id numero
+    # auth_tool = aws2.CognitoIdentityProviderWrapper(boto3.client('cognito-idp', 'us-east-1'), user_pool_id=user_pool_id, client_id=client_id)
 
     # to create a user. Send a confirmation code to the given email
     # read the username from the frontend
@@ -96,15 +96,16 @@ def tapahtumat_home():
     # confirm = auth_tool.confirm_user_sign_up(user, confirmation_code=confirm_code)  # returns true if okay
     # to be continued
 
-    try:
-        response = s3.list_objects_v2(Bucket=BUCKET_NAME, Delimiter="/")
-        response = response['CommonPrefixes']
-        for item in response:
-            folders[item['Prefix'][:-1]] = 0  # return the "folders"
-    except:
-        folders['Malaga_2022'] = 0
-        folders['Kimmo_mokki_2021'] = 0
-        pic = 'https://galleryeteenpain.s3.us-east-1.amazonaws.com/Malaga_2022/malaga_ryhma.jpg?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEIr%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCmV1LW5vcnRoLTEiRzBFAiEAs7dHrFGx3N4h1hOufEKMRbzTrzUJ8NJH5zSAeuWwnlcCIDAkbD%2BsfGbJCWJDMlpfbwKJjSHqUrZm%2FEVlqLtkCPXJKuQCCHMQABoMNTU5NTg4MzM3MzE4IgyqTUKyaoYCrO6%2FllsqwQKIjQxPzmOCnAwTsHXEErxlzkqk6VoG6J8Ip68z87sxl0Wn8woY4J1kymUiqdJvA48EnpWWvjDK9wYeQHV2%2FukjiMVuyAtp0rNi4EhD2pk8PBis0Hhlx1ewHFkis4aNQ8SXuosgIXcrQwL8byR2Ie737%2FMAg11aEyfyX91dT0wNUaQEIUMQcMlcDYzOoxK50QOjgJkvId3A1TjBrLD0ujSpRCXUUpa9%2BHc92EO2FUS%2BJ%2BKv%2BuUZpsMT8NbKo1XZhT8WzLnsTA2f0yF1v9axOfEl2UAvah44hvAZXo1p76sI4htEHkRd2bdeFDOC5irGrex610B0a71B5XmaeQnvhGbshiUoQ%2FkbNeVG3MIHf6BJ3XxyWAAX%2FYoKkFyM4%2B6VhoRt6h8icW98dM1MdSjto2v0c1ayfJr48L0qMPOHOS3WINUw3Jn%2BmgY6swIze0xlP0xuLIxmzQ3C8OS7DVwnUYDCMoTG8Hw7qZKtcARkwf3WXr6xS8uyHZNOZhW9NB%2Flqfh4L5fcMD%2FS8mMZhq88j%2B%2BETGTZsfDFLgpZiGjW%2Fn%2Fe2d8HYFVLFTI5swaGdq%2B4BPUkpvoiygiOzJHj832Ve4kpVVnjFK39KWBi3%2BJTwU630vIa1ypi9YpjjhudBr%2FyJk6xe9wZbmk7Faw3V%2FYhwfx%2B0ksntkmZ3IKHJaAw6JtbZih%2B1UOSJj3%2B%2BPbucy6CI3wNB2F2wjIHZg0hcyrGtg3zyueZedorrGsF5H0cO2j4IjIfWHdsmlniidLKDzUFGXd1mA%2BNian5qXaDwgk9JYRb14aL87pb7ruLCIYXHBxVlhv2YkEMNya6DxBF6aV4ftxATc4Ns0QUCcga5446&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20221031T131754Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43200&X-Amz-Credential=ASIAYESQQM2TODRAWNXC%2F20221031%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=bcb5f0dc2c95408c40444eeb443b80708a1bb7511056740277f40d92ea43801a'
+    # try:
+    #    response = s3.list_objects_v2(Bucket=BUCKET_NAME, Delimiter="/")
+    #    response = response['CommonPrefixes']
+    #    for item in response:
+    #        folders[item['Prefix'][:-1]] = 0  # return the "folders"
+    # except:
+    folders['Malaga_2022'] = 0
+    folders['Kimmo_mokki_2021'] = 0
+    pic = images[1]
+    # pic = 'https://galleryeteenpain.s3.us-east-1.amazonaws.com/Malaga_2022/malaga_ryhma.jpg?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEIr%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCmV1LW5vcnRoLTEiRzBFAiEAs7dHrFGx3N4h1hOufEKMRbzTrzUJ8NJH5zSAeuWwnlcCIDAkbD%2BsfGbJCWJDMlpfbwKJjSHqUrZm%2FEVlqLtkCPXJKuQCCHMQABoMNTU5NTg4MzM3MzE4IgyqTUKyaoYCrO6%2FllsqwQKIjQxPzmOCnAwTsHXEErxlzkqk6VoG6J8Ip68z87sxl0Wn8woY4J1kymUiqdJvA48EnpWWvjDK9wYeQHV2%2FukjiMVuyAtp0rNi4EhD2pk8PBis0Hhlx1ewHFkis4aNQ8SXuosgIXcrQwL8byR2Ie737%2FMAg11aEyfyX91dT0wNUaQEIUMQcMlcDYzOoxK50QOjgJkvId3A1TjBrLD0ujSpRCXUUpa9%2BHc92EO2FUS%2BJ%2BKv%2BuUZpsMT8NbKo1XZhT8WzLnsTA2f0yF1v9axOfEl2UAvah44hvAZXo1p76sI4htEHkRd2bdeFDOC5irGrex610B0a71B5XmaeQnvhGbshiUoQ%2FkbNeVG3MIHf6BJ3XxyWAAX%2FYoKkFyM4%2B6VhoRt6h8icW98dM1MdSjto2v0c1ayfJr48L0qMPOHOS3WINUw3Jn%2BmgY6swIze0xlP0xuLIxmzQ3C8OS7DVwnUYDCMoTG8Hw7qZKtcARkwf3WXr6xS8uyHZNOZhW9NB%2Flqfh4L5fcMD%2FS8mMZhq88j%2B%2BETGTZsfDFLgpZiGjW%2Fn%2Fe2d8HYFVLFTI5swaGdq%2B4BPUkpvoiygiOzJHj832Ve4kpVVnjFK39KWBi3%2BJTwU630vIa1ypi9YpjjhudBr%2FyJk6xe9wZbmk7Faw3V%2FYhwfx%2B0ksntkmZ3IKHJaAw6JtbZih%2B1UOSJj3%2B%2BPbucy6CI3wNB2F2wjIHZg0hcyrGtg3zyueZedorrGsF5H0cO2j4IjIfWHdsmlniidLKDzUFGXd1mA%2BNian5qXaDwgk9JYRb14aL87pb7ruLCIYXHBxVlhv2YkEMNya6DxBF6aV4ftxATc4Ns0QUCcga5446&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20221031T131754Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43200&X-Amz-Credential=ASIAYESQQM2TODRAWNXC%2F20221031%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=bcb5f0dc2c95408c40444eeb443b80708a1bb7511056740277f40d92ea43801a'
     return render_template("/public/templates/tapahtumat.html", message=folders, pic=pic)
 
 
