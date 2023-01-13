0249b14f6a1d289ec56a6909c44c2ef7bdee9cd3
leikeri <aki.jarvenpaa@gmail.com>
2023-01-13 12:27:48 +0100

    formatting and small fixes

--- database_tools.py
+++ database_tools.py
@@ -3,31 +3,21 @@ from models import Users
 
 db.create_all()
 
-# Fetch all data to users
-users = Users.query.all()
-# print columns
-for user in users:
-    print(user.username)
-    # print(user.reg_date)
-    print(f"<id={user.id}, username={user.username}>")
-
-
-# Inserts records into a mapping table
-def insert_user(user_obj):
-    db.session.add(user_obj)
-
 
 # delete records from a table
 def delete_user(username):
     user_obj = Users.query.filter_by(username=username).first()
-    db.session.delete(user_obj)
+    try:
+        db.session.delete(user_obj)
+    except:
+        print('Käyttäjätunnusta ei ole tietokannassa')
     db.session.commit()
     db.session.flush()
 
 
 if __name__ == '__main__':
 
-    name = 'Laita kayttajanimi tahan'
+    name = 'Rooster'
     delete_user(name)
     users = Users.query.all()
     # print columns
@@ -35,4 +25,3 @@ if __name__ == '__main__':
         print(user.username)
         # print(user.reg_date)
         print(f"<id={user.id}, username={user.username}>")
-
